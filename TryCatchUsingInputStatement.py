e2aadcaa35f0a3b47f56a063731ebff95e97d577
abhishek1722 <48902733+abhishek1722@users.noreply.github.com>
2020-05-30 14:57:22 +0530

    Update TryCatchUsingInputStatement.py

--- TryCatchUsingInputStatement.py
+++ TryCatchUsingInputStatement.py
@@ -1,3 +1,8 @@
+'''
+The try block lets you test a block of code for errors.
+The except block lets you handle the error.
+The finally block lets you execute code, regardless of the result of the try- and except blocks.
+'''
 #try catch using input statement
 
 a=int(input("Enter first number :"))
