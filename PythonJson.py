dd1b3b840f66f14004bedc881af6909dabe19b89
abhishek1722 <48902733+abhishek1722@users.noreply.github.com>
2020-05-31 19:23:57 +0530

    Update PythonJson.py

--- PythonJson.py
+++ PythonJson.py
@@ -16,8 +16,8 @@ y = json.loads(x)
 # the result is a Python dictionary:
 print(y["age"])         # output : 30
 print(y["name"])        # output : Toshi
-print(y["city"])        #Pune
-print(y)                #{'name': 'Toshi', 'age': 30, 'city': 'Pune'}
+print(y["city"])        # output : Pune
+print(y)                # output : {'name': 'Toshi', 'age': 30, 'city': 'Pune'}
 
 
 #Convert from Python to JSON:
